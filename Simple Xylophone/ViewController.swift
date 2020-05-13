8cc3828998a2dd99e74d7c7a5f6c8577dd923963
Demario Douce <demario.douce567@gmail.com>
2020-05-13 16:47:56 -0400

    Update design.

--- Simple Xylophone/ViewController.swift
+++ Simple Xylophone/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        //Reduces the sender's (the button that got pressed) opacity to half.
+         sender.alpha = 0.5
+
+         //Code should execute after 0.2 second delay.
+         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+             //Bring's sender's opacity back up to fully opaque.
+         sender.alpha = 1.0
+         }
+             
+         let letterPressed = sender.currentTitle
+         playSound(Pressed: letterPressed!)
+    }
+    
+    func playSound(Pressed:String?) {
+        
+    
+        
+            let url = Bundle.main.url(forResource: Pressed , withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+            
+
+        
+    }
 }
 
